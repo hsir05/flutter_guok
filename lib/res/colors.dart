44c8dd6bac276b2a2b2a4a19e147c02aed0af8fe
hdq <haqsir@163.com>
2020-02-26 20:59:05 +0800

    res provide

---
+++ lib/res/colors.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Colours {
+  static const Color app_main = Color(0xffb4282d);
+  static const Color dark_app_main = Color(0xFF3F7AE0);
+
+  static const Color bg_color = Color(0xFFf4f7fc);
+  static const Color dark_bg_color = Color(0xFF18191A);
+
+  static const Color bg_detail_color = Color(0xFF4c4c4b);
+  static const Color btn_border_color = Color(0xFFfda533);
+
+  static const Color material_bg = Color(0xFFFFFFFF);
+  static const Color dark_material_bg = Color(0xFF303233);
+
+  static const Color text = Color(0xFF333333);
+  static const Color dark_text = Color(0xFFB8B8B8);
+
+  static const Color text_star = Color(0xFFe09015);
+
+  static const Color text_green = Color(0xFF37ad26); // 展开
+
+  static const Color bg_avatar = Color(0xFF346d43);
+
+  static const Color icon_heart = Color(0xFFff4055);
+  static const Color icon_bg = Color(0xFF6a6a6a);
+
+  static const Color text_gray = Color(0xFF999999);
+  static const Color dark_text_gray = Color(0xFF666666);
+
+  static const Color text_gray_c = Color(0xFFcccccc);
+  static const Color dark_button_text = Color(0xFFF2F2F2);
+  
+  static const Color bg_gray = Color(0xFFF6F6F6);
+  static const Color dark_bg_gray = Color(0xFF1F1F1F);
+
+  static const Color line = Color(0xFFEEEEEE);
+  static const Color dark_line = Color(0xFF3A3C3D);
+
+  static const Color red = Color(0xFFFF4759);
+  static const Color dark_red = Color(0xFFE03E4E);
+  
+  static const Color text_disabled = Color(0xFFD4E2FA);
+  static const Color dark_text_disabled = Color(0xFFCEDBF2);
+
+  static const Color button_disabled = Color(0xFF96BBFA);
+  static const Color dark_button_disabled = Color(0xFF83A5E0);
+
+  static const Color unselected_item_color = Color(0xffbfbfbf);
+  static const Color dark_unselected_item_color = Color(0xFF4D4D4D);
+
+  static const Color bg_gray_ = Color(0xFFFAFAFA);
+  static const Color dark_bg_gray_ = Color(0xFF242526);
+
+
+}
